5e01de12fe529cb28a32514f072dcd1c512a0667
Akshay <akshaypsanap@gmail.com>
2022-07-15 03:10:46 +0530

    Commit

--- src/main/java/com/csi/core/Welcome.java
+++ src/main/java/com/csi/core/Welcome.java
@@ -4,7 +4,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Nashik");
 
-		System.out.println("I Have To Add New Feature");
+		System.out.println("I Have To Add New Feature");		
 	}
 
 }
