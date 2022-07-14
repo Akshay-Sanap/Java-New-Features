b29c6aedf30a0ff14931bcce1ec62b047df87ecd
Akshay <akshaypsanap@gmail.com>
2022-07-15 03:14:06 +0530

    Branch Feature Added

--- src/main/java/com/csi/core/Welcome.java
+++ src/main/java/com/csi/core/Welcome.java
@@ -4,7 +4,10 @@ public class Welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Nashik");
 
-		System.out.println("I Have To Add New Feature");		
+		System.out.println("I Have To Add New Feature");
+		
+		System.out.println("Hello Akshay");
+
 	}
 
 }
